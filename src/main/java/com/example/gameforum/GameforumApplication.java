a3f80064e25629b11b67d3ff3c10cd4ae9660d11
Maksim Lukin <maximlm93@gmail.com>
2025-07-24 17:33:06 +0500

    first commit

---
+++ src/main/java/com/example/gameforum/GameforumApplication.java
@@ -0,0 +1,13 @@
+package com.example.gameforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameforumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameforumApplication.class, args);
+	}
+
+}
