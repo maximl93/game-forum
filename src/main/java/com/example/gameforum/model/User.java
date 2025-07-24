ea9e9bd5f9b544179ef52ae51ca6cf31454a73cd
Maksim Lukin <maximlm93@gmail.com>
2025-07-24 17:58:17 +0500

    add getter and setter to User

--- src/main/java/com/example/gameforum/model/User.java
+++ src/main/java/com/example/gameforum/model/User.java
@@ -1,7 +1,14 @@
 package com.example.gameforum.model;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class User {
 
     private long id;
     private String username;
+
 }
