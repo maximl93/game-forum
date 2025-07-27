955f5c810dfda75e8bbe11b600647497fbd381ca
Maksim Lukin <maximlm93@gmail.com>
2025-07-27 15:59:51 +0500

    create User repository

---
+++ src/main/java/com/example/gameforum/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.gameforum.repository;
+
+import com.example.gameforum.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
