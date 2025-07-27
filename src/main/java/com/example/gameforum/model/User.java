0b602864dab6fdd659d010b44ccdcc2cb8229edb
Maksim Lukin <maximlm93@gmail.com>
2025-07-27 15:47:02 +0500

    add id generation and jpa annotation for User

--- src/main/java/com/example/gameforum/model/User.java
+++ src/main/java/com/example/gameforum/model/User.java
@@ -1,14 +1,27 @@
 package com.example.gameforum.model;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
+@Entity
+@Table(name = "users")
 @Getter
 @Setter
 public class User {
 
+    @Id
+    @SequenceGenerator(name = "user_sequence", sequenceName = "user_id_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_sequence")
     private long id;
+
+
     private String username;
 
 }
